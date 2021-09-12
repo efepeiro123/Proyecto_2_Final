08896a2631d54b67e219ae4069eb0bbb0d22afb5
Luis-RasconCalderon <87144287+Luis-RasconCalderon@users.noreply.github.com>
2021-09-11 22:57:41 -0600

    Create ModificadorCSV.java

---
+++ ModificadorCSV.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;  
+
+public class ModificadorCSV {
+    private String estadoARchivo;
+    private String encabezado;
+
+
+
+    public ModificadorCSV(int resultados){
+
+    }
+}
