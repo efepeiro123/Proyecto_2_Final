aff40861c6af8c1b58f05583a649e2dc94b300d2
efepeiro123 <evelynflpeiro@gmail.com>
2021-09-11 23:06:39 -0600

    Create TestPsico.java
    Clase TestPsico creada con sus metodos y atributos 11/09/2021

---
+++ TestPsico.java
@@ -0,0 +1,23 @@
+public class TestPsico {
+    //Atributos
+    private String[] ListaPsico;
+    private String[] ListaPsicoRespuestas;
+
+
+    //Constructor
+    public TestPsico(){
+
+    }
+    
+    //Metodos
+
+    //--->Getters
+    public String[] getListaPsico() {
+        return ListaPsico;
+    }
+
+    //--->Getters
+    public String[] getListaPsicoRespuestas() {
+        return ListaPsicoRespuestas;
+    }
+}
