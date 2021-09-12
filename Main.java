1befe481f4b390d5a095a2f2846044cfe8c0df2a
efepeiro123 <evelynflpeiro@gmail.com>
2021-09-11 22:35:43 -0600

    Create Main.java
    Clase Main creada 11/09/2021

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //Controlador ctrl = new Controlador(); //Llama al controlador
+        //ctrl.menu(); //Se muestra el menu
+    }
+}
