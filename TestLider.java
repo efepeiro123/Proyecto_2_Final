f60099f1553f46563e4e32fcd2ab546113bcc421
SpyingRaptor01 <ros21313@uvg.edu.gt>
2021-09-12 19:54:20 -0600

    Create TestLider.java

---
+++ TestLider.java
@@ -0,0 +1,17 @@
+public class TestLider{
+    //Atributos__________________________________________________________
+    private String[] ListaLid;
+    private String[] ListaLidRespuestas;
+
+    //Constructor________________________________________________________
+    public TestLider(){
+
+    }
+    //Metodos getter____________________________________________________
+    public String[] getListaLid(){
+        return ListaLid;
+    }
+    public String[] getListaLidRespuestas(){
+        return ListaLidRespuestas;
+    }
+}
