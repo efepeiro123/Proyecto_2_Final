dff68714b3271ef72cecdd87d5778257de4e15c9
KevinOtnielLopezAvalos21049 <87377189+KevinOtnielLopezAvalos21049@users.noreply.github.com>
2021-09-12 20:26:04 -0600

    Desarrollo de la Vista
    Atributos y métodos de la Vista.

---
+++ Vista.java
@@ -0,0 +1,47 @@
+/**
+ * <h1> Proyecto Integrador - Programa de contratación 
+ * <h2> Programación Orientada a Objetos </h1>
+ * @author
+ * @since 12-septiembre-2021
+ * @version 1.0
+ * @category Vista 
+ */
+ 
+  import java.util.Scanner;
+ class Vista{
+	 // Atributos
+	 Scanner scan = new Scanner();
+	 // Constructores y getters
+	 
+	 // Metodos
+	 public Vista(){
+		 
+	 }
+	 public concientizacion (){
+		 
+	 }
+	 public mostrarTest(String[]test){
+		 
+	 }
+	 public nuevoAspirante(){
+		 
+	 }
+	 public salir(){
+		 
+	 }
+	 public bienvenidad(){
+		 
+	 }
+	 public invalido(){
+		 
+	 }
+	 public menu(){
+		 
+	 }
+	 public verAspirantes(){
+		 
+	 }
+	 public setCSV(){
+		 
+	 }
+}
