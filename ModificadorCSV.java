408320a04f21aa9b745a814f29873d844b1c2797
Luis-RasconCalderon <87144287+Luis-RasconCalderon@users.noreply.github.com>
2021-09-16 01:03:38 -0600

    Update ModificadorCSV.java
    Se desarrolló el método para almacenar los datos de un nuevo aspirante el cuál ha sido ingresado al sistema. Esto luego de realizar su examen y obtener su calificación.

--- ModificadorCSV.java
+++ ModificadorCSV.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.BufferedReader;
-import java.io.IOException;  
+import java.io.IOException; 
+import java.util.ArrayList;; 
 
 //La clase contendrá los archivos CSV de los aspirantes y contraseñas de los empleadores.
 public class ModificadorCSV {
@@ -11,8 +13,8 @@ public class ModificadorCSV {
 
 
 
-    public ModificadorCSV(int resultados){
-
+    public ModificadorCSV(){
+        
     }
 
 
@@ -34,7 +36,29 @@ public class ModificadorCSV {
 
     }
 
-    public void agregarAspirante(String[] datos){
+    
+
+    //Almacena los datos del nuevo aspirante ingresado al sistema. 
+    public void agregarAspirante(String[] datosPersona){
+        ArrayList<String[]> aspirantesArray = new ArrayList<String[]>();
+        aspirantesArray.add(datosPersona);
+        File aspirantes = new File("aspirantes.csv"); //Nombre del archivo CSV el cual contendrá a los aspirantes
+			try{
+				FileWriter fw = new FileWriter(aspirantes);
+				BufferedWriter bw = new BufferedWriter(fw);
+                bw.write("DPI"+","+"Nombre"+","+"Número"+"Correo"+"Calificación");//Encabezado
+				bw.newLine();
+				for(int o=0;o<aspirantesArray.size();o++){
+					bw.write(aspirantesArray.get(o)[0]+","+aspirantesArray.get(o)[1]+","+aspirantesArray.get(o)[2]+","+aspirantesArray.get(o)[3]);
+					bw.newLine();
+				}
+				bw.close();
+				fw.close();
+					
+			}
+			catch(IOException e){
+				e.printStackTrace(); 
+			}
 
     }
 
