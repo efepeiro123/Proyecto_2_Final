66d84fc91144799d8871f616d02dbcf69287fd83
efepeiro123 <evelynflpeiro@gmail.com>
2021-09-15 23:15:40 -0600

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+
 public class Sistema {
-    //Atributos__________________________________________________________
+    
+    //Atributos
+    private TestLider testLider;
     private ArrayList<String> respuestasLidTemp;
     private ArrayList<String> respuestasPsicoTemp;
     private Random rand;
@@ -11,18 +14,24 @@ public class Sistema {
     private String[] preguntasPsico;
     private String[] respuestasPsico;
     
-    //Constructor________________________________________________________
+    //Constructor
     //Revisar los returns porque solo hay un tipo de respuesta porque son 
     //son tipos de respuesta pero en el diseño no lo escribimos asi 
     // y ver el return del CSV
     public Sistema(){
+        rand = new Random(); 
+        testLider = new TestLider();
+        };
         this.respuestasLidTemp = new ArrayList<String>();
         this.respuestasPsicoTemp = new ArrayList<String>();
     }
+
     public String[] generarTestLid(){
+        int max = 
         return preguntasLid;
     }
     public String[] generarTestPsico(){
+
         return preguntasPsico;
     }
 
