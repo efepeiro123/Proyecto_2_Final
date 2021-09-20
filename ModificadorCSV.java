6272ef8fe06ba4e44fb04dc57c420b359ee34afb
Luis-RasconCalderon <87144287+Luis-RasconCalderon@users.noreply.github.com>
2021-09-19 23:22:09 -0600

    Update ModificadorCSV.java
    Se completó la forma para poder conservar datos del archivo csv cada vez que se inicializa le programa y se ingresa un nuevo aspirante.

--- ModificadorCSV.java
+++ ModificadorCSV.java
@@ -5,15 +5,17 @@ import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.IOException; 
 import java.util.ArrayList;
+
+
+
 import java.io.FileNotFoundException;
 
 
 //La clase contendrá los archivos CSV de los aspirantes y contraseñas de los empleadores.
 public class ModificadorCSV {
     private String estadoARchivo;
-    private String encabezado;
-    private File aspirantes = new File("/Proyecto_2_Final/aspirantesNuevo.csv"); //Nombre del archivo CSV el cual contendrá a los aspirantes
-
+    private File path = new File("/Proyecto_2_Final/aspirantesNuevo.csv"); //Nombre del archivo CSV el cual contendrá a los aspirantes
+ 
 
     public ModificadorCSV(){
         
@@ -37,50 +39,56 @@ public class ModificadorCSV {
     public void limpiarCompleto(){
 
     }
-
+    /**
+     * Se encarga de guardar los datos por fila en un arraylist el cual luego será utilizado por guardar los datos del nuevo aspirante.
+     * @return aspirantesarreglo Es el ArrayList que guarda los datos de las filas del CSV.
+     */
+    public ArrayList<String[]> prepararCSV(){
+        ArrayList<String[]> aspirantesarreglo = new ArrayList<String[]>();
+		String line = "";
+		
+		try{
+            BufferedReader br = new BufferedReader(new FileReader(path));
+			while((line = br.readLine())!= null){
+				String[] values = line.split(",");
+				aspirantesarreglo.add(values);
+			}
+            br.close();
+			
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+        return aspirantesarreglo;
+    }
     
 
-    //Almacena los datos del nuevo aspirante ingresado al sistema. 
+    /**
+     *  Almacena los datos del nuevo aspirante ingresado al sistema utilizando como base el arralist enviado por el metodo prepararCSV 
+     * @param datosPersona Son los datos del nuevo aspirante ingresado en el controlador y vista.
+     */
+    
     public void agregarAspirante(String[] datosPersona){
-        
-       
-			try{
-                ArrayList<String[]> aspirantesArray = new ArrayList<String[]>();
-                String [] encabezado = {"DPI","Nombre","Numero","Correo","Calificacion"};
-                aspirantesArray.add(encabezado);
-				FileWriter fw = new FileWriter(aspirantes);
-				BufferedWriter bw = new BufferedWriter(fw);
-               /*  FileReader fr = new FileReader(aspirantes);
-                BufferedReader br = new BufferedReader(fr); */
-                String line = "";
-                BufferedReader br = new BufferedReader(new FileReader(aspirantes));
-                System.out.println("aaaaa");
-                System.out.println(line = br.readLine());
-                System.out.println("aaaaa");
-                while((line = br.readLine())!= null){
-                    String[] values = line.split(",");
-                    aspirantesArray.add(values);
+        ArrayList<String[]> aspirantesArray = new ArrayList<String[]>();
+        aspirantesArray = prepararCSV();
+        aspirantesArray.add(datosPersona);
+        try{
+            FileWriter fw = new FileWriter(path);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for(int o=0;o<aspirantesArray.size();o++){
+                bw.write(aspirantesArray.get(o)[0]+","+aspirantesArray.get(o)[1]+","+aspirantesArray.get(o)[2]+","+aspirantesArray.get(o)[3]+","+aspirantesArray.get(o)[4]);
+                if(o < (aspirantesArray.size()-1)){
+                    bw.newLine();
                 }
-                br.close();
-                
-                aspirantesArray.add(datosPersona);
-                
-				for(int o=0;o<aspirantesArray.size();o++){
-					bw.write(aspirantesArray.get(o)[0]+","+aspirantesArray.get(o)[1]+","+aspirantesArray.get(o)[2]+","+aspirantesArray.get(o)[3]+","+aspirantesArray.get(o)[4]);
-					if(o < (aspirantesArray.size()-1)){
-                        bw.newLine();
-                    }
-				}
-				bw.close();
-				fw.close();
-               
-
-					
-			}catch(IOException e){
-				e.printStackTrace(); 
-			}
+            }
+            bw.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
 
     }
+        
 
     // public String mostrarAspirtantes(){
 
