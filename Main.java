091b1e105519c7e76443edde7447ba641f10a7db
KevinOtnielLopezAvalos21049 <lop21049@uvg.edu.gt>
2021-09-30 11:02:08 -0600

    Actualización del encabezado de las clases.
    Actualizaciones.

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
+/**
+ * <h1> Proyecto Integrador -  sistema de contratación 
+ * <h2> Programación Orientada a Objetos </h1>
+ * @author 
+ * @since3 3-octubre-2021
+ * @version 2.0
+ * @category Modelo
+ */
+
 //import java.util.Scanner;
 
 public class Main {
