e35f307f8593bb0887e21542dce1c92d9fcf4d93
efepeiro123 <evelynflpeiro@gmail.com>
2021-10-19 10:38:06 -0600

    Actualización de sistema
    Actualización de la clase sistema y eliminación de clase testLider y testPsico

--- Sistema.java
+++ Sistema.java
@@ -1,4 +1,4 @@
-/**
+/** 
  * <h1> Proyecto Integrador -  sistema de contratación 
  * <h2> Programación Orientada a Objetos sección 41</h1>
  * <h3> Sistema (Forma parte del Modelo) </h2>
@@ -16,77 +16,64 @@
  * @version 1.0
  * @category Modelo 
  */
-
- // Paquete defaults de java. 
 import java.util.ArrayList;
 import java.util.Random;
 
 
-public class Sistema {
+public abstract class Sistema {
     //Atributos
-    private TestLider testLider;
-    private ArrayList<String> respuestasLidTemp;
-    //private ArrayList<String> respuestasPsicoTemp;
-    private Random rand;
-    private String[] preguntasLid = new String[1];
-    private String[] respuestasLid = new String[1];
-
-   // private String[] preguntasPsico;
-   // private String[] respuestasPsico;
+    protected Test test = new Test();
+    protected ArrayList<String> respuestasTemp = new ArrayList<String>();
+    //protected ArrayList<String> respuestasPsicoTemp = new ArrayList<String>();
+    protected Random rand = new Random();
+    protected String[] preguntas = new String[1];
+    protected String[] respuestas = new String[1];
+   // protected String[] preguntasPsico;
+   // protected String[] respuestasPsico;
     
     //Constructor
     //Revisar los returns porque solo hay un tipo de respuesta porque son 
     //son tipos de respuesta pero en el diseño no lo escribimos asi 
     // y ver el return del CSV
-
-
     public Sistema(){
-        rand = new Random(); 
-        testLider = new TestLider();
-        respuestasLidTemp = new ArrayList<String>();
+        //rand = new Random(); 
+        //test = new Test();
+        //respuestasTemp = new ArrayList<String>();
         //this.respuestasPsicoTemp = new ArrayList<String>();
     }
 
-    public String[] generarTestLid(){
-        preguntasLid = testLider.getListaLid();
-        respuestasLid = testLider.getListaLidRespuestas();
-        int max = preguntasLid.length - 1; 
-        int min = 0;
-        ArrayList<Integer> repetidos = new ArrayList<Integer>();
-        repetidos.clear();
-        int preguntasSolicitadas = 10;
-        String [] obtenerPreguntas = new String[preguntasSolicitadas];
-        for (int i = 0; i < preguntasSolicitadas; i++){
-            Integer seleccionador = rand.nextInt((max - min ) + 1) + min;
-            if(repetidos.contains(seleccionador)==false){
-                repetidos.add(seleccionador); 
-                obtenerPreguntas[i] = preguntasLid[seleccionador];
-                respuestasLidTemp.add(respuestasLid[seleccionador]);
-           }else{
-               i--;
-            }
-        }
-
-        
-        return obtenerPreguntas;
-    }
-    // public String[] generarTestPsico(){
-
-    //     return preguntasPsico;
-    // }
-
-    public String recibirRespuestasLid(String [] respuestasLid){
-        int punteo = 0;
-
-        for (int i = 0; i < respuestasLid.length; i++) {
-            if(respuestasLidTemp.get(i).equals(respuestasLid[i])){
-                punteo += 1;
-            }
-        }
-
-        respuestasLidTemp.clear();
-        return Integer.toString(punteo);
-    }
+    //public String[] generarTestLid(){
+    //    preguntas = test.getListaLid();
+    //    respuestas = test.getListaLidRespuestas();
+    //    int max = preguntas.length - 1; 
+    //    int min = 0;
+//
+    //    int preguntasSolicitadas = 10;
+    //    String [] obtenerPreguntas = new String[preguntasSolicitadas];
+    //    for (int i = 0; i < preguntasSolicitadas; i++) {
+    //        int seleccionador = rand.nextInt((max - min ) + 1) + min; 
+    //        obtenerPreguntas[i] = preguntas[seleccionador];
+    //        respuestasTemp.add(respuestas[seleccionador]);
+    //    }
+    //    return obtenerPreguntas;
+    //}
+    //// public String[] generarTestPsico(){
+//
+    ////     return preguntasPsico;
+    //// }
+//
+    //public String recibirRespuestas(String [] respuestasLid){
+    //    int punteo = 0;
+//
+    //    for (int i = 0; i < respuestasLid.length; i++) {
+    //        if(respuestasTemp.get(i).equals(respuestasLid[i])){
+    //            punteo += 1;
+    //        }
+    //    }
+//
+    //    respuestasTemp.clear();
+    //    return Integer.toString(punteo);
+    //}
     
     // public String[] recibirRespuestasPsico(){
     //     return respuestasLid;
