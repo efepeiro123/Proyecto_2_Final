749e551e8cfaa5a028965c408816fc73b940a7b0
KevinOtnielLopezAvalos21049 <lop21049@uvg.edu.gt>
2021-10-21 12:19:45 -0600

    Deasrrollo del punteo.

--- CalificadorPsico.java
+++ CalificadorPsico.java
@@ -6,11 +6,13 @@ interface SistemaCalificador{
 public class CalificadorPsico extends Sistema implements SistemaCalificador{
     
     public String [] recibirRespuestas(String [] respuestasPsi, ArrayList<String> respuestasCorrectasPsico){
+        int punteo = 0; 
     for (int i = 0; i < respuestasPsi.length; i++) {
         if(respuestasCorrectasPsico.get(i).equals(respuestasPsi[i])){
             punteo += 1;
         }
     }
+    return Interger.toString(punteo);
 }
 // Override del método generarTest ()
 }
